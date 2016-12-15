f8de81edef8c343e6b4ca4494a0d49a45cf1ef69
David Castelblanco B <david.castelblanco@hotmail.com>
2016-12-15 12:14:47 -0500

    Implemented a module to create the message an load all the attachments.

--- cli.py
+++ cli.py
@@ -12,10 +12,33 @@
     --version     Show version.
 
 """
-
+import os
 from docopt import docopt
+from smtplib import SMTP
+from email import encoders
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+
+from message import createMessage
 
 options = docopt(__doc__)
 
 if 'send' in options:
+
   print('This guy knows what he\'s doing. I like that.')
+
+  msg = {}
+  msg['From'] = 'david.castelblanco@hotmail.com'
+  msg['To'] = 'cbdavides@gmail.com'
+  msg['Subject'] = 'Buena buena'
+  msg['Attachments'] = [os.path.join('.', filename) for filename in options['<file>']]
+
+  text = createMessage(msg)
+
+  server = SMTP('smtp-mail.outlook.com', 587)
+  server.starttls()
+  server.login(msg['From'], 'Spectre@Freaks95')
+
+  server.sendmail(msg['From'], msg['To'], text)
+
+  server.quit()
