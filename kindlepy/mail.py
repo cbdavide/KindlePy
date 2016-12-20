db80a8a5e3750a89c3a59237fb38c09d48282db9
David Castelblanco B <david.castelblanco@hotmail.com>
2016-12-19 23:07:06 -0500

    Implemented support for some popular smtp services, some of them yet to be tested.

--- kindlepy/mail.py
+++ kindlepy/mail.py
@@ -1,9 +1,38 @@
 import os
+import re
 from smtplib import SMTP
+from smtplib import SMTP_SSL
 from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
+
+services = {
+    'google': {
+        'smtp': 'smtp.gmail.com',
+        'domains': ['gmail', 'googlemail'],
+        'port': 465,
+        'secure': True
+    },
+    'microsoft': {
+        'smtp': 'smtp-mail.outlook.com',
+        'domains': ['outlook', 'hotmail'],
+        'port': 587,
+        'secure': False
+    },
+    "yahoo": {
+        'smtp': "smtp.mail.yahoo.com",
+        'domains': ['yahoo'],
+        'port': 465,
+        'secure': True
+    },
+    'aol': {
+        'smtp': "smtp.aol.com",
+        'domains': ['aol.com'],
+        'port': 587,
+        'secure': False
+    }
+}
 
 def createMessage(conf, attachments):
 
@@ -30,10 +59,34 @@ def buildAttachment(path):
 
     return attachment
 
+def findSMTPServer(emailDomain):
+    '''
+        Look through all the services to get a domain that matches with emailDomain
+        an return a tuple with its smtp hots, and smtp port
+    '''
+    for key, service in services.items():
+        if emailDomain in service['domains']:
+            return service['smtp'], service['port'], service['secure']
+
+    return None, None
+
+def getDomain(emailAddress):
+    return re.search(r'@(\w+)', emailAddress).groups()[0]
+
 def sendMail(conf, message):
+    host, port, secure = findSMTPServer(getDomain(conf['from']))
 
-    with SMTP('smtp-mail.outlook.com', 587) as smtp:
-        print(smtp.starttls())
-        print(smtp.login(conf['from'], conf['pass']))
+    # TODO: Raise an exception.
+    if not host:
+        return 'Error'
 
-        return smtp.sendmail(conf['from'], conf['to'], message)
+    if secure:
+        with SMTP_SSL(host, port) as smtp:
+            print(smtp.login(conf['from'], conf['pass']))
+            return smtp.sendmail(conf['from'], conf['to'], message)
+    else:
+        with SMTP(host, port) as smtp:
+            print(smtp.starttls())
+            print(smtp.login(conf['from'], conf['pass']))
+
+            return smtp.sendmail(conf['from'], conf['to'], message)
