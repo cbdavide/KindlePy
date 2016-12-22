fd323e1f39277bf8b3a6a55dbffcb46b363d4874
David Castelblanco B <david.castelblanco@hotmail.com>
2016-12-22 18:00:23 -0500

    Changed the path of the user config.

--- kindlepy/src/config.py
+++ kindlepy/src/config.py
@@ -1,9 +1,15 @@
 import re
+import os
 from validate_email import validate_email
 
+CONFIG_FILE_PATH = os.path.expanduser('~/.config/kindlepy')
+CONFIG_FILE_NAME = 'config'
+
 def readConfig():
+    config_file = prepareConfigFile()
     conf = {}
-    with open('data', 'r') as input:
+
+    with open(config_file, 'r') as input:
         data = input.readlines()
         data = [x.strip('\n') for x in data]
 
@@ -14,7 +20,8 @@ def readConfig():
         return conf
 
 def writeConfig(conf):
-    with open('data', 'w') as output:
+    config_file = prepareConfigFile()
+    with open(config_file, 'w') as output:
         for key, value in conf.items():
             output.write('{}={}{}'.format(key, value, '\n'))
 
@@ -25,3 +32,16 @@ def set(kind, email):
         writeConfig(conf)
     else:
         raise Exception('It seems that the email does not have a valid syntax.')
+
+def prepareConfigFile():
+    '''
+        Checks whether or not the config file is exist
+        and returns the absolute path of the file
+        (The config file is created in case that it does not exist)
+    '''
+    file_path = os.path.join(CONFIG_FILE_PATH, CONFIG_FILE_NAME)
+    if not os.path.exists(CONFIG_FILE_PATH):
+        os.makedirs(CONFIG_FILE_PATH)
+        with open(file_path, 'w') as fp:
+            pass
+    return file_path
